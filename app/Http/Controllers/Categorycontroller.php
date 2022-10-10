813ffd1035c5fb4dfc8632d5404dbd57c792e3b5
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-10-10 20:14:57 +0900

    Revert "wip"
    This reverts commit 3add2bb107f8415c2098b696ca8fca18ec456d10.

--- app/Http/Controllers/Categorycontroller.php
+++ app/Http/Controllers/Categorycontroller.php
@@ -2,45 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\Tag;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class Categorycontroller extends Controller
 {
-
-    public function index()
-    {
-        $categories = Category::query()
-            ->withCount('posts')->paginate(100);
-
-        return view('categories.index', compact('categories'));
-    }
-
-    public function show(Category $category)
-    {
-        $recent_posts = Post::query()
-            ->latest()
-            ->take(5)
-            ->get();
-
-        $categories = Category::query()
-            ->withCount('posts')
-            ->orderBy('posts_count', 'desc')
-            ->get();
-
-        $tags = Tag::query()
-            ->take(50)
-            ->get();
-
-        return view('categories.show', [
-            'recent_posts' => $recent_posts,
-            'categories' => $categories,
-            'tags' => $tags,
-
-            'category' => $category,
-            'posts' => $category->posts()->paginate(10),
-        ]);
-    }
+    //
 }
