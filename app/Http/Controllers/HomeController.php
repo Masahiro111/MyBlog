7f0408ad26be93db5c2d2f52c71ba1dfd61f9360
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-10-10 20:15:46 +0900

    Revert "サイドバー 「カテゴリー」と「タグ」の表示"
    This reverts commit ae9591a0f54d0f186397b25ce6ce57f283186a99.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -20,20 +18,6 @@ class HomeController extends Controller
             ->take(5)
             ->get();
 
-        $categories = Category::query()
-            ->withCount('posts')
-            ->orderBy('posts_count', 'desc')
-            ->get();
-
-        $tags = Tag::query()
-            ->take(50)
-            ->get();
-
-        return view('home', compact(
-            'posts',
-            'recent_posts',
-            'categories',
-            'tags',
-        ));
+        return view('home', compact('posts', 'recent_posts'));
     }
 }
