00f97c1618cac65c9189b885cfd2b1629ae5c763
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-10-10 20:15:56 +0900

    Revert "ホームページの記事表示"
    This reverts commit eb22f7709ab06418fa59b1c4c50703ba59ac2200.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/post', function () {
     return view('post');
