55ff29993909b6a7990556377f1c31d2c0527e82
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-10-12 18:16:03 +0900

    マスターレイアウトの作成とシングルページの作成

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/post', function () {
+    return view('post');
+})->name('post');
+
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
